944d90aaf47602991b39dbf7923f84edbcde1d9a
jism-coder <jaismora@hotmail.com>
2022-03-04 12:43:17 -0600

    GridView parte 1

---
+++ WebApplication1/Controles/ConsultaProductos.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConsultaProductos.aspx.cs" Inherits="WebApplication1.Controles.ConsultaProductos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Consulta productos</div>
+        <asp:GridView 
+            ID="idGvProductos"
+            runat="server"
+            AutoGenerateColumns="false"
+            >
+            <Columns>
+
+
+                <asp:BoundField DataField="PK_Producto" HeaderText="No de producto" />
+                <asp:BoundField DataField="Producto" HeaderText="Producto" />
+
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Controles/ConsultaProductos.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.Modelos;
+
+namespace WebApplication1.Controles
+{
+    public partial class ConsultaProductos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getProductos();
+        }
+        protected void getProductos()
+        {
+            List<ModeloProductos> query = new List<ModeloProductos>();
+            Consultas.Consultas c = new Consultas.Consultas();
+            query = c.getProductos();
+
+            if (query.Count > 0)
+            {
+
+                idGvProductos.AllowPaging = true;
+
+                idGvProductos.DataSource = query;
+
+                idGvProductos.DataBind();
+
+            }
+        }
+    }
+}
